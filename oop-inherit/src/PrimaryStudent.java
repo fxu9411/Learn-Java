72f4bb02c519d78fb4a5336aeef432a08f2b9afd
Frank Xu <franklolxu@gmail.com>
2021-11-03 21:58:14 -0400

    Remove unwanted files

--- oop-inherit/src/PrimaryStudent.java
+++
@@ -1,9 +0,0 @@
-
-public class PrimaryStudent extends Student {
-	protected int grade;
-	
-	public PrimaryStudent(String name, int age, int score, int grade) {
-		super(name, age, score);
-		this.grade = grade;
-	}
-}
--- spring-ioc-customize/src/main/java/com/itranswarp/learnjava/validator/Validator.java
+++
@@ -1,6 +0,0 @@
-package com.itranswarp.learnjava.validator;
-
-public interface Validator {
-
-	void validate(String email, String password, String name);
-}
