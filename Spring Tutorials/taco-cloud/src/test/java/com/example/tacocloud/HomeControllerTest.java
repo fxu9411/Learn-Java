723ed42e8494ddf4a83c056a293fba6888295c0d
Frank Xu <franklolxu@gmail.com>
2022-01-04 15:43:32 -0500

    Taco Cloud Finish Chapter 2

--- Spring Tutorials/taco-cloud/src/test/java/com/example/tacocloud/HomeControllerTest.java
+++ Spring Tutorials/taco-cloud/src/test/java/com/example/tacocloud/HomeControllerTest.java
@@ -1,6 +1,6 @@
 package com.example.tacocloud;
 
-import com.example.tacocloud.Controller.HomeController;
+import com.example.tacocloud.Web.WebConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest(WebConfig.class)
 public class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
