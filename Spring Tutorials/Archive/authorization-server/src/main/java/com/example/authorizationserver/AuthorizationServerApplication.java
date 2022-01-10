f11c8f464ee6387575f885cdfcbd8bb84b51d000
Frank Xu <franklolxu@gmail.com>
2022-01-10 13:23:13 -0500

    Update Structure

---
+++ Spring Tutorials/Archive/authorization-server/src/main/java/com/example/authorizationserver/AuthorizationServerApplication.java
@@ -0,0 +1,28 @@
+package com.example.authorizationserver;
+
+import com.example.authorizationserver.Model.User;
+import com.example.authorizationserver.Model.UserRepository;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+public class AuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner dataLoader(
+            UserRepository repo, PasswordEncoder encoder) {
+        return args -> {
+            repo.save(
+                    new User("habuma", encoder.encode("password"), "ROLE_ADMIN"));
+            repo.save(
+                    new User("tacochef", encoder.encode("password"), "ROLE_ADMIN"));
+        };
+    }
+}
